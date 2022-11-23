adeff1f40b02264615002216a8ef33820a01a411
0xf00ff00f <fzorto@gmail.com>
2022-11-23 20:18:46 -0300

    Add texture atlas and font cache

---
+++ texture.cc
@@ -0,0 +1,74 @@
+#include "texture.h"
+#include "pixmap.h"
+
+#include <memory>
+
+namespace
+{
+constexpr GLenum Target = GL_TEXTURE_2D;
+
+GLenum toGLFormat(PixelType pixelType)
+{
+    return pixelType == PixelType::RGBA ? GL_RGBA : GL_LUMINANCE;
+}
+
+GLenum toGLInternalFormat(PixelType pixelType)
+{
+    return pixelType == PixelType::RGBA ? GL_RGBA : GL_LUMINANCE;
+}
+}
+
+Texture::Texture(const Pixmap &pixmap)
+    : Texture(pixmap.width, pixmap.height, pixmap.pixelType, pixmap.pixels.data())
+{
+}
+
+Texture::Texture(int width, int height, PixelType pixelType, const unsigned char *data)
+    : m_width(width)
+    , m_height(height)
+    , m_pixelType(pixelType)
+{
+    initialize();
+    if (data)
+        setData(data);
+}
+
+Texture::~Texture()
+{
+    glDeleteTextures(1, &m_id);
+}
+
+void Texture::initialize()
+{
+    glGenTextures(1, &m_id);
+
+    bind();
+
+    glTexParameteri(Target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(Target, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(Target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(Target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    glTexImage2D(Target, 0, toGLInternalFormat(m_pixelType), m_width, m_height, 0, toGLFormat(m_pixelType),
+                 GL_UNSIGNED_BYTE, nullptr);
+
+    m_data.resize(m_width * m_height * pixelSizeInBytes(m_pixelType), 0);
+}
+
+void Texture::setData(const unsigned char *data) const
+{
+    std::copy(data, data + m_data.size(), m_data.data());
+    gpuSetData(data);
+}
+
+void Texture::gpuSetData(const unsigned char *data) const
+{
+    bind();
+    glTexSubImage2D(Target, 0, 0, 0, m_width, m_height, toGLFormat(m_pixelType), GL_UNSIGNED_BYTE, data);
+}
+
+void Texture::bind() const
+{
+    glBindTexture(Target, m_id);
+}
---
+++ texture.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "abstracttexture.h"
+#include "pixeltype.h"
+
+#include <GL/glew.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+struct Pixmap;
+
+class Texture : public AbstractTexture
+{
+public:
+    explicit Texture(const Pixmap &pixmap);
+    Texture(int width, int height, PixelType pixelType, const unsigned char *data = nullptr);
+    ~Texture() override;
+
+    void setData(const unsigned char *data) const;
+
+    int width() const { return m_width; }
+    int height() const { return m_height; }
+
+    void bind() const override;
+
+private:
+    void initialize();
+    void gpuSetData(const unsigned char *data) const;
+
+    int m_width;
+    int m_height;
+    PixelType m_pixelType;
+    GLuint m_id;
+    mutable std::vector<unsigned char> m_data;
+};
